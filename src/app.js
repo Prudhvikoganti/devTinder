cdaa75d3e690d9ec45aa8e5a329577450c992a43
Prudhvi Srivatsav Koganti <prudhvi@Prudhvis-MacBook-Air.local>
2026-06-10 14:39:56 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -6,13 +6,38 @@ app.get('/', (req, res) => (
     res.send('Hello World!')
 ))
 
-app.get('/about', (req, res) => (
-    res.send('This is the about page')
+app.get('/users/:userId/:name', (req, res) => (
+    console.log(req.params),
+    res.send('This is the users page')
 ))
 
-app.get('/contact', (req, res) => (
-    res.send('This is the contact page')
-))
+//Query 
+// app.get('/users', (req, res) => (
+//     console.log(req.query),
+//     res.send('This is the users page')
+// ))
+
+// app.get(/a(bc)?d/, (req, res) => {
+//     res.send('This route matches "/ad" and "/abcd"')
+// })
+
+// app.get(/.*fly$/, (req, res) => (
+//     res.send('This route matches any path that ends with "fly"')
+// ))
+
+// app.get('/about', (req, res) => (
+//     res.send('This is the about page')
+// ))
+
+// app.get('/contact', (req, res) => (
+//     res.send('This is the contact page')
+// ))
+
+// app.post('/about', (req, res) => (
+//     res.send('Form submitted successfully')
+// ))
+
+
 
 
 app.listen(3000,() => {
